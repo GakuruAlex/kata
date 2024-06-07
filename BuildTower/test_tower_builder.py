2bc526b9fc69b6872d814f4382b60aa956412b46
alexgicimo <gakurualexgicimo@gmail.com>
2024-06-07 13:00:34 +0000

    Testing tower_builder function

---
+++ BuildTower/test_tower_builder.py
@@ -0,0 +1,18 @@
+import pytest
+from tower_builder import tower_builder
+
+@pytest.mark.parametrize("a, expected", [
+    (3,[
+  "  *  ",
+  " *** ",
+  "*****"]),
+    (2, [' * ', '***']),
+    (1, ['*' ])
+])
+
+def test_tower_builder(a, expected):
+    assert tower_builder(a) == expected
+
+@pytest.mark.xfail(reason="This test is expected to fail")
+def test_tower_builder_failure():
+    assert tower_builder(2) == ['***']
